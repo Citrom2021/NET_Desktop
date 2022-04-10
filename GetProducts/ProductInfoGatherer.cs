3b8f540b34c2ccc4c1e79c1e0fc5be7915bb1b5e
Citrom2021 <citronejoe0123@citromail.hu>
2022-04-10 13:46:49 +0200

    .NET

---
+++ GetProducts/ProductInfoGatherer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+
+
+namespace GetProducts
+{
+    public partial class ProductInfoGatherer : Form
+    {		
+
+        public ProductInfoGatherer()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HttpClient clint = new HttpClient();
+            clint.BaseAddress = new Uri("http://localhost:8000/");
+            HttpResponseMessage response = clint.GetAsync("jsonproducts").Result;
+            var product = response.Content.ReadAsAsync<IEnumerable<Products>>().Result;
+            dataGridView1.DataSource = product;
+        }
+    }
+
+    
+}
---
+++ GetProducts/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetProducts
+{
+    class Products
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+
+        public string category { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+
+        public string created_at { get; set; }
+
+        public string updated_at { get; set; }
+    }
+}
